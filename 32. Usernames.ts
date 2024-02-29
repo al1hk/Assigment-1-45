88598e3c04d23453f1696b96e1502c22e16d3d12
al1hk <160332226+al1hk@users.noreply.github.com>
2024-02-29 23:29:59 +0500

    32. Usernames.ts

--- 32. Usernames.ts
+++ 32. Usernames.ts
@@ -1,10 +1,14 @@
-let current_users : string[] = ['Bob','David','John','Ethan','Gary']
-if(current_users.includes(current_users[]))
-{
-    console.log("This username is already taken!")
-
-}
-else{
-    console.log("This username is available for you to use!")
-
-}
+let current_users : string[] = ['Ahmed','Ayesha','Amna','Bashar']
+let new_users : string[] = ["Raza","Nadia","Ahmed","Rana","Ayesha"]
+let current_user_lower : string[] = current_users.map(user=> user.toLowerCase())
+for(let new_user of new_users)
+    {
+        if(current_user_lower.includes(new_user.toLowerCase())
+        ){
+            console.log(`Sorry ${new_user} name is already taken!`)
+        }
+        else
+        {
+            console.log(`Yes ${new_user} name is available for you to use!`)
+        }
+    }
