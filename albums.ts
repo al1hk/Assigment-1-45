29665bc67724bf87cb2213c79b1f13dbcd48e7c5
al1hk <160332226+al1hk@users.noreply.github.com>
2024-03-12 16:21:47 +0500

    albums.ts

--- albums.ts
+++ albums.ts
@@ -1,15 +1,12 @@
-function makeAlbum (artist:string, title:string) : {artist: string, title: string}
+function makeAlbum (artist:string, title:string)
 {
-    const details = {
-        artist : artist.charAt(0).toUpperCase() + artist.slice(1)
-        ,title : title.charAt(0).toUpperCase() + title.slice(1)
-    };
-    return details;
+    
+         return (`Artist Name: ${artist}, Song Name: ${title}`)
     
 }
-var album = makeAlbum('Weeknd','Out Of Time')
-console.log(album)
 var album = makeAlbum("Tale Impala","Let it Happen")
 console.log(album)
 var album = makeAlbum("Weeknd","Starboy")
+console.log(album)
+var album = makeAlbum("Future","Mask Off")
 console.log(album)
