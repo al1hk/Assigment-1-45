45eb83053308c2d27427d02e9e5cbe084b1f44a5
al1hk <160332226+al1hk@users.noreply.github.com>
2024-03-13 01:29:39 +0500

    30. admin.ts

--- 30. admin.ts
+++ 30. admin.ts
@@ -1,6 +1,5 @@
 let username : string[] = ["Admin",'Eric',"Bob",'John','Stacy']
 let admin_message : string = ", would you like to see today's report?"
-export{}
 if(username.includes('Admin'))
 {
     console.log('Hey ' + username[0] + admin_message);
